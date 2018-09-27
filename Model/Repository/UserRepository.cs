9c1a9bafe8bbf19113331ec4c3753d1e4d380734
jean-seb <ecnerual432@gmail.com>
2018-09-26 20:33:33 -0400

    user repo

---
+++ Model/Repository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using tunder.Model.DbContext;
+
+namespace tunder.Model.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ITunderDbContext _dbContext;
+
+        public UserRepository(ITunderDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public User GetById(long id)
+        {
+            return _dbContext.Users.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
