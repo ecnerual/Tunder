66155e8726fbd21cdf0787082ca0e471324555a6
Jean-Sébastien Dufour-Turmel <ecnerual432@gmail.com>
2018-10-09 20:48:43 -0400

    rip db

---
+++ Tunder/BusinessObject/Requests/UserSignInRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tunder.BusinessObject.Requests
+{
+    public class UserSignInRequest
+    {
+        [Required] public string Name { get; set; }
+    }
+}
---
+++ Tunder/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tunder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
