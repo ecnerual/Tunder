53f7d6d6877dee71fcb17a607fad1dd4b86c5c00
Jean-Sébastien Dufour-Turmel <ecnerual432@gmail.com>
2018-10-09 21:06:07 -0400

    fixed that docker!

---
+++ Data/BusinessObject/DTO/UserSignInRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tunder.BusinessObject.Requests
+{
+    public class User
+    {
+        [Required] public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Tunder/Services/IAuthService.cs
+++ Tunder/Services/IAuthService.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using tunder.Model;
 
 namespace tunder.Services
 {
     public interface IAuthService
     {
+        Task<User> Register(); 
       
     }
 }
