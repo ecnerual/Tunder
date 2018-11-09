a083fb180f16468ed6aefe203c2ed8e38670dc8a
jean-seb <ecnerual432@gmail.com>
2018-11-08 22:17:54 -0500

    login css

--- tunder-spa/src/views/Login.vue
+++ tunder-spa/src/views/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="view__container login__container">
-    <div class="box is-half">
+    <i class="logo fab fa-algolia fa-4x"></i>
+    <div class="box is-half login__form">
       <form  @submit="onSubmit">
         <div class="field">
           <p class="control has-icons-left has-icons-right">
@@ -21,19 +22,29 @@
         </div>
         <div class="field">
           <p class="control has-icons-left has-icons-right">
-            <input v-model="form.password" class="input" type="password" placeholder="Password">
+            <input v-model="form.password" 
+                   v-validate="'required|min:6'" 
+                   class="input" 
+                   name="password"
+                   type="password" 
+                   placeholder="Password">
             <span class="icon is-small is-left">
               <i class="fas fa-lock"></i>
             </span>
           </p>
         </div>
+        <div class="forgot-password__container">
+          <router-link class="link" to="/ForgotPassword">Forgot password?</router-link>
+        </div>
         <div class="field is-grouped login__btn-group">
-          <div class="control is-right">
-            <button :class="{ 'is-loading': submitting }"  class="button is-primary">Login</button>
-          </div>
+          <button :class="{ 'is-loading': form.submitting }" :disabled="inError"  class="button is-primary">Login</button>
         </div>
       </form>
     </div>
+    <div class="box is-half">
+      <p>Ready for some action ? <router-link class="link" to="/signup">Register!</router-link></p>
+    </div>
+    
   </div>
 </template>
 
@@ -44,10 +55,15 @@ export default {
   data: () => ({
     form: {
       email: '',
-      password: ''
-    },
-    submitting: false
+      password: '',
+      submitting: false
+    }
   }),
+  computed: {
+    inError() {
+      return Object.keys(this.fields).some(key => this.fields[key].invalid);
+    }
+  },
   methods: {
     onSubmit() {
       const { email, password } = this;
@@ -65,22 +81,44 @@ export default {
 
 .field.is-grouped.login__btn-group {
   justify-content: flex-end;
+}
 
+.forgot-password__container {
+  text-align: right;
+  .link {
+    font-size: 0.75rem;
+  }
 }
 
 .login__container {
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+
+  .logo {
+    font-size: 5em;
+    margin-bottom: 10px;
+  }
+
+  .box {
+    padding: 1rem;
+  }
+
+  .button {
+    width: 100%;
+  }
 
   .is-half {
-    transform: translatey(-26px);
-    height: 250px;
     width: 50%;
+    max-width: 350px;
     display: flex;
     align-items: center;
     justify-content: center;
 
+    &.login__form {
+      height: 250px;
+    }
     @media screen and (max-width: 1024px) {
       width: 88%;
     }
