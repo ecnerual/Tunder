6b979cb7b822d9aa8a75623e9ffe34e529eb05d4
jean-seb <ecnerual432@gmail.com>
2018-11-10 23:32:11 -0500

    static types

--- tunder-spa/src/store/modules/session.js
+++ tunder-spa/src/store/modules/session.js
@@ -1,18 +1,28 @@
 const axios = require('axios');
 
+export const LOGIN = 'ACTON_LOGIN';
+
+export const SET_TOKEN = 'SET_TOKEN';
+
+const types = {
+  SET_TOKEN,
+  LOGIN
+}
+
 const state = {
-  token: null
+  token: null,
+  isAuth: false
 };
 
 const actions = {
-  login({ commit }, payload) {
+  [LOGIN]({ commit }, payload) {
     const { email, password } = payload; 
     axios.post('http://localhost:35396/api/session/login', {
       email,
       password
     })
     .then(res => {
-      commit('setToken', res.data.token);
+      commit(types.SET_TOKEN, res.data.token);
     })
     .catch(err => console.error(err));
   }
@@ -21,8 +31,9 @@ const actions = {
 const getters = {};
 
 const mutations = {
-  setToken(state, newToken) {
+  [SET_TOKEN](state, newToken) {
     state.token = newToken;
+    state.isAuth = true;
   }
 };
 
@@ -31,5 +42,6 @@ export default {
   state,
   getters,
   actions,
-  mutations
-}
+  mutations,
+  types
+};
--- tunder-spa/tests/unit/store/modules/session.spec.js
+++ tunder-spa/tests/unit/store/modules/session.spec.js
@@ -1,17 +1,24 @@
 import store from '@/store/modules/session.js'
 import { expect } from 'chai'
 
+describe('@/store/modules/sessions', () => {
+  let state;
 
-describe('Describe session mutations', () => {
-  const state = {
-    token: null
-  };
+  describe('Mutations', () => {
+    const { mutations, types } = store;
 
-  const { mutations } = store;
+    beforeEach(() => {
+      state = store.state;
+    });
 
-  mutations.setToken(state, 'madame');
+    it('Set token should set token', () => {
+      mutations[types.SET_TOKEN](state, 'madame');
+      expect(state.token).to.equal('madame');
+    })
 
-  it('Set the token', () => {
-    expect(state.token).to.equal('madame');
-  });
+    it('Set token should set isAuth', () => {
+      mutations[types.SET_TOKEN](state, 'madame');
+      expect(state.isAuth).to.equal(true);
+    })
+  })
 });
